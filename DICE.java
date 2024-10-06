a756ff2bf1cfa59e0499a459bd0cf2f8fcfe8f5b
Asia Harris <asia.harris1@bulldogs.aamu.edu>
2024-10-05 20:52:02 -0500

    Edits to Test.java and Dice.java
    Test.java - Instructions are added
    Dice.java - Mutator, accessor, DiceArray, and RollOnce methods are all created and completed

--- DICE.java
+++ DICE.java
@@ -1,19 +1,43 @@
+import java.util.Arrays;
+import java.util.Random;
 /**
 * @author Asia Harris
 * @version 10/5/2024
 */
+
 public class Dice {
     private int sides;
+    private Random random;
 
     public Dice (int sides){
         this.sides=sides;
+        this.random = new Random(); //Intializes the Random object
     }
 
     //Write a mutator method for the instance variable
-
+    public void setSides(int sides){
+        this.sides = sides;
+    }
     //Write an accessor method for the instance variable
-
+    public int getSides(){
+        return sides;
+    }
     //Write a DiceArray Method to roll your Dice object once
+    //Parameter will be the number of rolls;
+    //The return with be an array holding the results sorted from low to high
+    public int[] DiceArray(int numRolls){
+        int[] result = new int[numRolls];
+
+        for(int i = 0;  i < numRolls; i++){
+            result[i]= RollOnce(); //Rolls the dice and stores the result
+        }
+        Arrays.sort(result); //Sorts the result array from low to high
+        return result;  //Returns sorted result
+    }
+    //Write a RollOnce Method to roll your Dice object once
     //No parameter
     //Return will be the integer Dice value
+    public int RollOnce(){
+        return random.nextInt(sides) + 1; //Generate a random number between 1 and sides
+    }
 }
--- Test.java
+++ Test.java
@@ -5,5 +5,31 @@
 public class Test {
     public static void main (String[] args){
         
+        //create two dice objects with 6 sides numbered 1-6
+
+        System.out.println("Risk Simulator Experiment");
+        ///Call static method with a parameter of 10,000
+
+        System.out.println("DIstribution Experiment with 2 standard dice 1-6");
+        //Call static method with a prameter of 10,000
+
+        System.out.println("Distribution Experiment with 2 non-standard dice 1-8");
+        //Mutate existing two dice to be eight sides
+        //Call static method with a parameter of 10,000
+
+        /*Risk Simulator Experiment
+          Write a static method that will roll the first Dice object three times (attacker) and store the results in a sorted
+          array. Then roll the second Dice object two times (defender). The winner is decided by comparing the
+          results of the highest roll of defender vs attacker and awarding the winner a point and then comparing the
+          second highest roll of defender vs attacker and awarding the winner a point. Should the dice results be
+          equal, it is a tie with no winner. Run this experiment 10,000 times and then print the results (See output
+          example in the project guide) You will call the DiceArray Method you are created in the Dice Class
+          */
+        /* Distribution Experiment
+          Write a static method that will roll the two Dice objects once each and add the two dice. Run this experiment
+          10,000 times and then print the results. (See output example in the project guide) You will call the RollOnce
+          Method you created in the Dice Class. Hint: Keep your counts in an array instead of separate variables as
+          the output will vary with the number of sides.
+        */
     }
 }
