2639ffffa121343f43441b95fe438274447bf457
Asia Harris <asia.harris1@bulldogs.aamu.edu>
2024-10-05 20:34:38 -0500

    Instructions are added to Dice Class and the Main Java Class is deleted

---
+++ DICE.java
@@ -0,0 +1,19 @@
+/**
+* @author Asia Harris
+* @version 10/5/2024
+*/
+public class Dice {
+    private int sides;
+
+    public Dice (int sides){
+        this.sides=sides;
+    }
+
+    //Write a mutator method for the instance variable
+
+    //Write an accessor method for the instance variable
+
+    //Write a DiceArray Method to roll your Dice object once
+    //No parameter
+    //Return will be the integer Dice value
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+/**
+* @author Asia Harris
+* @version 10/5/2024
+*/
+public class Test {
+    public static void main (String[] args){
+        
+    }
+}
